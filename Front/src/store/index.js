cfc797b0d55f2768de936db0662ed14e3e4ab98d
EunjiYi <eunji0yi@gmail.com>
2021-02-04 11:59:55 +0900

    feature/profile과 conflict해결

--- Front/src/store/index.js
+++ Front/src/store/index.js
@@ -17,7 +17,28 @@ const state = {
     password: "",
     nickname: "",
     myteams: []
-  }
+  },
+  myTeams: [
+    {name: "T1", img: "https://placekitten.com/300/300" , nickname: "", followers: 14565, category: "lol"},
+    {name: "대구FC", img: "https://placekitten.com/300/300" , nickname: "", followers: 1234, category: "football"},
+    {name: "삼성라이온즈", img: "https://placekitten.com/300/300" , nickname: "", followers: 76567, category: "baseaball"},
+  ],
+  teams: [
+    {name: "T1", img: "https://placekitten.com/300/300" , nickname: "", followers: 14565, category: "lol"},
+    {name: "GEN.G", img: "https://placekitten.com/300/300" , nickname: "", followers: 14565, category: "lol"},
+    {name: "SSG 일렉트로스", img: "https://placekitten.com/300/300", nickname: "", followers: 14565, category: "baseball"},
+    {name: "롯데 자이언츠", img: "https://placekitten.com/300/300", nickname: "", followers: 14565, category: "baseball"},
+    {name: "FC서울", img: "https://placekitten.com/300/300", nickname: "", followers: 14565, category: "football"},
+    {name: "수원삼성 블루윙즈", img: "https://placekitten.com/300/300", nickname: "", followers: 14565, category: "football"},
+    {name: "DRX", img: "https://placekitten.com/300/300", nickname: "", followers: 14565, category: "lol"},
+    {name: "리브샌박", img: "https://placekitten.com/300/300", nickname: "", followers: 14565, category: "lol"},
+    {name: "전북현대", img: "https://placekitten.com/300/300", nickname: "", followers: 14565, category: "football"},
+    {name: "LG 트윈스", img: "https://placekitten.com/300/300", nickname: "", followers: 14565, category: "baseball"},
+    {name: "두산 베어스", img: "https://placekitten.com/300/300", nickname: "", followers: 14565, category: "baseball"},
+    {name: "한화생명", img: "https://placekitten.com/300/300", nickname: "", followers: 14565, category: "lol"},
+    {name: "대구FC", img: "https://placekitten.com/300/300", nickname: "", followers: 1234, category: "football"},
+    {name: "삼성라이온즈", img: "https://placekitten.com/300/300", nickname: "", followers: 76567, category: "baseball"},
+  ]
 };
 
 export default new Vuex.Store({
