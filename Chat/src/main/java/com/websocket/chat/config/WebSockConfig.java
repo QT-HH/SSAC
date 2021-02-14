46e6bef1c53bca4ea9e45d2b170a94d598f4c0d1
leegw217 <leegw217@naver.com>
2021-02-15 07:20:47 +0900

    0215 Back chat server update

--- Chat/src/main/java/com/websocket/chat/config/WebSockConfig.java
+++ Chat/src/main/java/com/websocket/chat/config/WebSockConfig.java
@@ -18,7 +18,7 @@ public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/").setAllowedOrigins("*")
+        registry.addEndpoint("/ws-stomp").setAllowedOrigins("*")
                 .withSockJS();
     }
 }
