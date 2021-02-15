a2f21fa0aa05ec3c2b15357ebee6ebd7e5c81fda
leegw217 <leegw217@naver.com>
2021-02-15 22:21:56 +0900

    0215 Back chat server update

--- Chat/src/main/java/com/websocket/chat/controller/ChatController.java
+++ Chat/src/main/java/com/websocket/chat/controller/ChatController.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @RequiredArgsConstructor
@@ -16,18 +16,15 @@ public class ChatController {
 	@Autowired
 	private ChatService chatService;
 
-    private final SimpMessageSendingOperations messagingTemplate;
-
-    @MessageMapping("/chat/message")
-    public void message(ChatMessage message) {
-        try {
-        	System.out.println("채팅 to server");
-			chatService.chatting(message);
-			System.out.println("채팅 to db");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
-        System.out.println("채팅 to client");
+    @MessageMapping("/receive")
+    
+    @SendTo("/send")
+    
+    public ChatMessage SocketHandler(ChatMessage message) throws Exception {
+        System.out.println("방번호 : "+message.getRoomId());
+        System.out.println("보낸사람 : "+message.getSender());
+        System.out.println("내용 : "+message.getMessage());
+        chatService.chatting(message);
+        return message;
     }
 }
