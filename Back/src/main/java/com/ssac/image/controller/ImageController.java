4c24b706947857ff2335a733c19fccebaa488093
leegw217 <leegw217@naver.com>
2021-02-17 03:18:28 +0900

    0216 Back Image update

--- Back/src/main/java/com/ssac/image/controller/ImageController.java
+++ Back/src/main/java/com/ssac/image/controller/ImageController.java
@@ -5,6 +5,10 @@ import com.ssac.image.service.ImageService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
@@ -12,97 +16,67 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 import java.io.IOException;
 import java.util.List;
 
-@Api(tags = {"1. File"})
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 @RequestMapping("/file")
-@CrossOrigin(origins = "*")
 public class ImageController {
+	@Autowired
+    private ImageService imageService;
 
-    private final ImageService service;
-
-    public FileController(FileService service){
-        this.service = service;
-    }
-
-    @ApiOperation(value="이미지 등록(upload)", notes = "이미지 파일을 업로드한다.", httpMethod = "POST")
-    @PostMapping(value = "/upload")
-    public Response uploadFile(
-            @ApiParam(value="MultiipartFile 형태의 이미지 배열") MultipartHttpServletRequest request)
-    {
-        Response response;
-
+    @PostMapping("/logo")
+    public ResponseEntity<?> uploadOne(MultipartFile file) throws Exception {
         try {
-            List<MultipartFile> inputs = request.getFiles("files");
-            List<Image> images = service.addFiles(inputs);
-
-            response = new Response("success", images.size()+"개 파일 등록 성공", images);
+            imageService.logoImage();
+            return new ResponseEntity<String>("success", HttpStatus.OK);
         } catch(Exception e){
-            return response = new Response("error","파일 등록 실패",e.getMessage());
+            e.printStackTrace();
         }
-
-        return response;
+        return new ResponseEntity<String>("fail", HttpStatus.OK);
     }
 
-    @ApiOperation(value="이미지 1개 등록(upload)", notes = "이미지 파일을 업로드한다.", httpMethod = "POST")
-    @PostMapping(value = "/upload1")
-    public Response uploadOne(
-            @ApiParam(value="MultiipartFile 파일") MultipartFile file) throws IllegalStateException {
-        Response response;
-
-        try {
-
-            response = new Response("success", "1개 파일 등록 성공", service.addFile(file));
-        } catch(Exception e){
-            System.out.println("sad");
-            response = new Response("error","파일 등록 실패",e.getMessage());
-        }
-
-        return response;
-    }
-
-    @ApiOperation(value="이미지 서빙(n개)", notes = "여러개의 이미지 불러오기", httpMethod = "GET")
-    @GetMapping(value = "/fileServe")
-    public Response fileServe(@ApiParam(value="파일 고유값 리스트") @RequestParam(value="fileIds",required = true) List<Integer> fileIds) throws IllegalStateException {
-        Response response;
-
-        try {
-            response = new Response("success", "파일 서빙 성공", service.filesServe(fileIds));
-        } catch(Exception e){
-            System.out.println("sad");
-            response = new Response("error","파일 서빙 실패",e.getMessage());
-        }
-
-        return response;
-    }
-
-    @ApiOperation(value="이미지 서빙(1개)", notes = "이미지 불러오기", httpMethod = "GET")
-    @GetMapping(value = "/fileServe/{fileId}")
-    public Response fileServe(@ApiParam(value="파일 고유값") @PathVariable int fileId) throws IllegalStateException {
-        Response response;
-
-        try {
-            response = new Response("success", "파일 서빙 성공", service.fileServe(fileId));
-        } catch(Exception e){
-            System.out.println("sad");
-            response = new Response("error","파일 서빙 실패",e.getMessage());
-        }
-
-        return response;
-    }
-
-    @ApiOperation(value="이미지 삭제", notes = "이미지 삭제하기", httpMethod = "DELETE")
-    @DeleteMapping(value = "/delete/{fileId}")
-    public Response delete(@ApiParam(value="파일 고유값") @PathVariable int fileId) throws IllegalStateException {
-        Response response;
-
-        try {
-            service.delete(fileId);
-            response = new Response("success", "파일 삭제 성공", null);
-        } catch(Exception e){
-            System.out.println("sad");
-            response = new Response("error","파일 삭제 실패",e.getMessage());
-        }
-
-        return response;
-    }
+//    @ApiOperation(value="이미지 서빙(n개)", notes = "여러개의 이미지 불러오기", httpMethod = "GET")
+//    @GetMapping(value = "/fileServe")
+//    public Response fileServe(@ApiParam(value="파일 고유값 리스트") @RequestParam(value="fileIds",required = true) List<Integer> fileIds) throws IllegalStateException {
+//        Response response;
+//
+//        try {
+//            response = new Response("success", "파일 서빙 성공", service.filesServe(fileIds));
+//        } catch(Exception e){
+//            System.out.println("sad");
+//            response = new Response("error","파일 서빙 실패",e.getMessage());
+//        }
+//
+//        return response;
+//    }
+//
+//    @ApiOperation(value="이미지 서빙(1개)", notes = "이미지 불러오기", httpMethod = "GET")
+//    @GetMapping(value = "/fileServe/{fileId}")
+//    public Response fileServe(@ApiParam(value="파일 고유값") @PathVariable int fileId) throws IllegalStateException {
+//        Response response;
+//
+//        try {
+//            response = new Response("success", "파일 서빙 성공", service.fileServe(fileId));
+//        } catch(Exception e){
+//            System.out.println("sad");
+//            response = new Response("error","파일 서빙 실패",e.getMessage());
+//        }
+//
+//        return response;
+//    }
+//
+//    @ApiOperation(value="이미지 삭제", notes = "이미지 삭제하기", httpMethod = "DELETE")
+//    @DeleteMapping(value = "/delete/{fileId}")
+//    public Response delete(@ApiParam(value="파일 고유값") @PathVariable int fileId) throws IllegalStateException {
+//        Response response;
+//
+//        try {
+//            service.delete(fileId);
+//            response = new Response("success", "파일 삭제 성공", null);
+//        } catch(Exception e){
+//            System.out.println("sad");
+//            response = new Response("error","파일 삭제 실패",e.getMessage());
+//        }
+//
+//        return response;
+//    }
 }
--- Back/src/main/java/com/ssac/image/service/ImageService.java
+++ Back/src/main/java/com/ssac/image/service/ImageService.java
@@ -5,142 +5,153 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.ssac.mappers.ImageMapper;
+import com.ssac.team.dto.Team;
+import com.ssac.team.service.TeamService;
 
 import javax.imageio.ImageIO;
-import javax.transaction.Transactional;
-import java.awt.image.BufferedImage;
 import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
 @Service
 public class ImageService {
+	@Autowired
+	public TeamService teamService;
+	@Autowired
+	public ImageMapper mapper;
+	
+	public void logoImage() throws Exception {
+		List<Team> teams = teamService.listTeam();
+		String ext = ".png";
+		String filename = "";
+		String pathRoot = "/home/image/logo";
+		for(int i=0; i<teams.size(); i++) {
+			String eventRoot = "";
+			if(teams.get(i).getEvent_no() == 1) eventRoot = "/football";
+			else if(teams.get(i).getEvent_no() == 2) eventRoot = "/baseball";
+			else eventRoot = "/lol";
+			String totalRoot = pathRoot + eventRoot;
+			File pFile = new File(totalRoot);
+			filename = teams.get(i).getName()+ext;
+			InputStream imgStream = new FileInputStream(totalRoot+filename);
+			byte[] imgByteArray = IOUtils.toByteArray(imgStream);
+			imgStream.close();
+			HashMap<String, Object> map = new HashMap<String, Object>();
+			map.put("name", teams.get(i).getName());
+			map.put("logo", imgByteArray);
+			mapper.insertLogo(map);
+		}
+	}
 
-    @Autowired
-    private FileRepository repository;
-
-
-    public List<Image> addFiles(List<MultipartFile> files) throws Exception {
-
-        System.out.println("파일 개수:"+files.size());
-        List<Image> images = new ArrayList<>();
-
-        for (MultipartFile file : files) {
-            images.add(addFile(file));
-        }
-
-        return images;
-    }
-
-    public Image addFile(MultipartFile file) throws Exception{
-        if(file.isEmpty())  throw new Exception("파일 등록 실패 (파일 객체 비었음)");
-        String originName = file.getOriginalFilename();
-        System.out.println(originName);
-        String ext = "";
-        int index = originName.lastIndexOf(".");
-        if(index!=-1){
-            ext = originName.substring(index);
-        }
-
-
-        //저장할 이름
-        String systemName = UUID.randomUUID().toString() + ext;
-
-        String currentTime = new SimpleDateFormat("yyyy/MM/dd").format(new Date());
-        StringTokenizer st = new StringTokenizer(currentTime,"/");
-        //저장될 경로
-        //String fsl = File.pathSeparator;
-        //String fsl = "\\";
-        String fsl="/";
-
-        StringBuilder pathRoot = new StringBuilder();
-        pathRoot.append("/home/upload/image");
-
-        StringBuilder modulePath = new StringBuilder();
-        modulePath.append(st.nextToken())
-                .append(fsl).append(st.nextToken())
-                .append(fsl).append(st.nextToken());
-
-        String totalPath = pathRoot.toString()+fsl+modulePath.toString();
-        File pFile = new File(totalPath);
-        //폴더 있는지 확인하고 폴더 생성
-        if(pFile.exists()==false) {
-            pFile.mkdirs();
-        }
-
-        //서버에 파일 생성하기
-        //pfile의 설정된 경로를 준다
-        file.transferTo(new File(pFile,systemName));
-
-        Image image = new Image();
-        image.setOriginName(originName);
-        image.setSystemName(systemName);
-        image.setType(file.getContentType());
-        image.setSize((int)file.getSize());
-        image.setPath(modulePath.toString());
-
-        Image doneImage = repository.save(image);
-        InputStream imgStream = new FileInputStream(totalPath+fsl+systemName);
-        byte[] imgByteArray = IOUtils.toByteArray(imgStream);
-        imgStream.close();
-
-        doneImage.setImageBytes(imgByteArray);
-
-        return doneImage;
-    }
-
-    public List<Image> filesServe(List<Integer> fileIds) throws IOException {
-        List<Image> images = new ArrayList<>();
-        for (Integer fileId : fileIds) {
-            images.add(fileServe(fileId));
-        }
-        return images;
-    }
-
-    public Image fileServe(int fileId) throws IOException {
-
-        Image image = repository.findById(fileId);
-
-        //String fsl = File.pathSeparator;
-        //String fsl = "\\";
-        String fsl="/";
-
-        StringBuilder path = new StringBuilder();
-        path.append("/home/upload/image");
-        //path.append("C:\\Users\\sumin\\etc\\upload\\eureka");
-
-        path.append(fsl).append(image.getPath())
-                .append(fsl).append(image.getSystemName());
-
-        InputStream imgStream = new FileInputStream(path.toString());
-        byte[] imgByteArray = IOUtils.toByteArray(imgStream);
-        imgStream.close();
-
-        image.setImageBytes(imgByteArray);
-
-        return image;
-    }
-
-    @Transactional
-    public void delete(int fileId) throws Exception {
-
-        Image image = repository.findById(fileId);
-        repository.deleteAllById(fileId);
-
-        //String fsl = File.pathSeparator;
-        //String fsl = "\\";
-        String fsl="/";
-
-        StringBuilder pathRoot = new StringBuilder();
-        pathRoot.append("/home/upload/image");
-
-        pathRoot.append(fsl).append(image.getPath())
-                .append(fsl).append(image.getSystemName());
-        File file = new File(pathRoot.toString());
-        if(file.exists()){
-            if(!file.delete())  throw new Exception("파일 삭제 실패");
-        }
-    }
+//    public Image addFile(MultipartFile file) throws Exception{
+//        if(file.isEmpty())  throw new Exception("파일 등록 실패 (파일 객체 비었음)");
+//        String originName = file.getOriginalFilename();
+//        System.out.println(originName);
+//        String ext = "";
+//        int index = originName.lastIndexOf(".");
+//        if(index!=-1){
+//            ext = originName.substring(index);
+//        }
+//
+//
+//        //저장할 이름
+//        String systemName = UUID.randomUUID().toString() + ext;
+//
+//        String currentTime = new SimpleDateFormat("yyyy/MM/dd").format(new Date());
+//        StringTokenizer st = new StringTokenizer(currentTime,"/");
+//        //저장될 경로
+//        //String fsl = File.pathSeparator;
+//        //String fsl = "\\";
+//        String fsl="/";
+//
+//        StringBuilder pathRoot = new StringBuilder();
+//        pathRoot.append("/home/upload/image");
+//
+//        StringBuilder modulePath = new StringBuilder();
+//        modulePath.append(st.nextToken())
+//                .append(fsl).append(st.nextToken())
+//                .append(fsl).append(st.nextToken());
+//
+//        String totalPath = pathRoot.toString()+fsl+modulePath.toString();
+//        File pFile = new File(totalPath);
+//        //폴더 있는지 확인하고 폴더 생성
+//        if(pFile.exists()==false) {
+//            pFile.mkdirs();
+//        }
+//
+//        //서버에 파일 생성하기
+//        //pfile의 설정된 경로를 준다
+//        file.transferTo(new File(pFile,systemName));
+//
+//        Image image = new Image();
+//        image.setOriginName(originName);
+//        image.setSystemName(systemName);
+//        image.setType(file.getContentType());
+//        image.setSize((int)file.getSize());
+//        image.setPath(modulePath.toString());
+//
+//        Image doneImage = repository.save(image);
+//        InputStream imgStream = new FileInputStream(totalPath+fsl+systemName);
+//        byte[] imgByteArray = IOUtils.toByteArray(imgStream);
+//        imgStream.close();
+//
+//        doneImage.setImageBytes(imgByteArray);
+//
+//        return doneImage;
+//    }
+//
+//    public List<Image> filesServe(List<Integer> fileIds) throws IOException {
+//        List<Image> images = new ArrayList<>();
+//        for (Integer fileId : fileIds) {
+//            images.add(fileServe(fileId));
+//        }
+//        return images;
+//    }
+//
+//    public Image fileServe(int fileId) throws IOException {
+//
+//        Image image = repository.findById(fileId);
+//
+//        //String fsl = File.pathSeparator;
+//        //String fsl = "\\";
+//        String fsl="/";
+//
+//        StringBuilder path = new StringBuilder();
+//        path.append("/home/upload/image");
+//        //path.append("C:\\Users\\sumin\\etc\\upload\\eureka");
+//
+//        path.append(fsl).append(image.getPath())
+//                .append(fsl).append(image.getSystemName());
+//
+//        InputStream imgStream = new FileInputStream(path.toString());
+//        byte[] imgByteArray = IOUtils.toByteArray(imgStream);
+//        imgStream.close();
+//
+//        image.setImageBytes(imgByteArray);
+//
+//        return image;
+//    }
+//
+//    public void delete(int fileId) throws Exception {
+//
+//        Image image = repository.findById(fileId);
+//        repository.deleteAllById(fileId);
+//
+//        //String fsl = File.pathSeparator;
+//        //String fsl = "\\";
+//        String fsl="/";
+//
+//        StringBuilder pathRoot = new StringBuilder();
+//        pathRoot.append("/home/upload/image");
+//
+//        pathRoot.append(fsl).append(image.getPath())
+//                .append(fsl).append(image.getSystemName());
+//        File file = new File(pathRoot.toString());
+//        if(file.exists()){
+//            if(!file.delete())  throw new Exception("파일 삭제 실패");
+//        }
+//    }
 
 
 }
