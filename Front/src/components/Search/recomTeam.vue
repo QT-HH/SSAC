2708e1be39caf72a8bd4e2aa0f11706a32fbfd3a
EunjiYi <eunji0yi@gmail.com>
2021-02-16 21:13:36 +0900

    검색부분 통신 부분 기능구현 완료

--- Front/src/components/Search/recomTeam.vue
+++ Front/src/components/Search/recomTeam.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="text-center mt-4">
-    <!-- {{ surveyAnswers }} -->
-    <!-- <div class="mx-5 py-2"><v-btn outlined color="error" x-large class="mt-3" @click="seeResult()"> 결과보기 </v-btn></div> -->
+    <h3>이 팀을 추천해요</h3>
+    팀명: {{ name }}
+    <br>
+    팀 팔로우수: {{ follownum }}
+    
     <div class="mx-5 py-2"><v-btn outlined color="error" x-large class="mt-3" @click="goNext()"> 다시하기 </v-btn></div>
-    <!-- {{ team }} -->
+
   </div>
 </template>
 
@@ -15,7 +18,8 @@ export default {
   name: "recomTeam",
   data: function() {
     return {
-      team: [],
+      name: '',
+      follownum: '',
     }
   },
   methods: {
@@ -53,7 +57,9 @@ export default {
     axios.get(`http://i4d102.p.ssafy.io:9000/ssac/search/recommend/`, params)
       .then(response => {
         console.log(response.data)
-        // this.team = response.data.items
+        this.name = response.data.name
+        this.follownum = response.data.count
+        this.logo = response.data.logo
         // this.searched = true // 유저검색결과 한 줄 
       })
       .catch(error => {
