db4f129d70de4f2acba8f3177ae78a4f95f1d47e
multicampus <op032@naver.com>
2021-02-17 01:31:29 +0900

    0217 친구의팀 UI 수정 및 푸터 투명도 조정

--- Front/src/components/Search/Question2.vue
+++ Front/src/components/Search/Question2.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="text-center mt-4">
-    <h4>2. 어떤 경기가 더 좋아요?</h4>
-    <div class="mx-5 py-2"><v-btn outlined color="error" x-large class="mt-3" @click="[saveQuestion(choices[0]), goNext()]"> {{ choices[0] }} </v-btn></div>
-    <div class="mx-5 py-2"><v-btn outlined color="error" x-large class="mt-3" @click="[saveQuestion(choices[0]), goNext()]"> {{ choices[1] }} </v-btn></div>
+    <v-container>
+    <h4
+    style="text-align:center;
+            font-weight: bold; 
+            font-size: 1.7em;">2. 어떤 경기가 더 좋아요?</h4>
+            <br>
+    <div class="mx-5 py-2"><v-btn width="80vw" :elevation="2" style="height: 15vh" outlined color="black" x-large class="mt-3" @click="[saveQuestion(choices[0]), goNext()]"> {{ choices[0] }} </v-btn></div>
+    <div class="mx-5 py-2"><v-btn width="80vw" :elevation="2" style="height: 15vh" outlined color="black" x-large class="mt-3" @click="[saveQuestion(choices[0]), goNext()]"> {{ choices[1] }} </v-btn></div>
+    </v-container>
+    <div 
+    class="mt-6 mb-5" 
+    style=" top: 0;"
+    width="90vw">
+      <v-img
+            :src="welcome.src"
+            class="rounded-pill"
+          >
+      </v-img>
+    </div>
   </div>
 </template>
 
@@ -13,6 +29,7 @@ export default {
   data: function() {
     return {
       choices: ['화끈한 경기력이었지만 졌잘싸', '꾸역꾸역이지만 이겼으니 만족!'],
+      welcome : { src : require("@/assets/images/welcome1.png") }
     }
   },
   methods: {
--- Front/src/components/Search/Question3.vue
+++ Front/src/components/Search/Question3.vue
@@ -1,9 +1,25 @@
 <template>
   <div class="text-center mt-4">
-    <h4>3. 어떤 팀을 더 선호하나요?</h4>
-    <div class="mx-5 py-2"><v-btn outlined color="error" x-large class="mt-3" @click="[saveQuestion(choices[0]), goNext()]"> {{ choices[0] }} </v-btn></div>
-    <div class="mx-5 py-2"><v-btn outlined color="error" x-large class="mt-3" @click="[saveQuestion(choices[0]), goNext()]"> {{ choices[1] }} </v-btn></div>
-    <div class="mx-5 py-2"><v-btn outlined color="error" x-large class="mt-3" @click="[saveQuestion(choices[0]), goNext()]"> {{ choices[2] }} </v-btn></div>
+    <v-container>
+    <h4
+    style="text-align:center;
+            font-weight: bold; 
+            font-size: 1.7em;">3. 어떤 팀을 선호하나요?</h4>
+            <br>
+    <div class="mx-5 py-2"><v-btn width="80vw" :elevation="2" outlined color="black" x-large class="mt-3" @click="[saveQuestion(choices[0]), goNext()]"> {{ choices[0] }} </v-btn></div>
+    <div class="mx-5 py-2"><v-btn width="80vw" :elevation="2" outlined color="black" x-large class="mt-3" @click="[saveQuestion(choices[0]), goNext()]"> {{ choices[1] }} </v-btn></div>
+    <div class="mx-5 py-2"><v-btn width="80vw" :elevation="2" outlined color="black" x-large class="mt-3" @click="[saveQuestion(choices[0]), goNext()]"> {{ choices[2] }} </v-btn></div>
+    </v-container>
+    <div 
+    class="mt-8 mb-5" 
+    style=" top: 0;"
+    width="90vw">
+      <v-img
+            :src="welcome.src"
+            class="rounded-pill"
+          >
+      </v-img>
+    </div>
   </div>
 </template>
 
@@ -14,6 +30,7 @@ export default {
   data: function() {
     return {
       choices: ['자주 이기는 상위권팀', '예측할 수 없는 중위권팀', '반전을 꿈꾸는 하위권 팀'],
+      welcome : { src : require("@/assets/images/welcome1.png") }
     }
   },
   methods: {
