fe678e226f79927aaf56c05725b7653e4f29136b
multicampus <op032@naver.com>
2021-02-18 02:26:57 +0900

    0218 Front modified Sports/news UI

--- Front/src/components/Sports/News.vue
+++ Front/src/components/Sports/News.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>
-    <br>
+  <div class="mt-7">
     <v-row align="center" justify="space-around">
-      <v-btn color="blue" @click="showFootball">축구</v-btn>
-      <v-btn color="blue" @click="showBaseball">야구</v-btn>
-      <v-btn color="blue" @click="showLOL"> e스포츠 </v-btn>
+      <v-card class="ml-6 rounded-lg white--text deep-purple lighten-4" width="30vw" @click="showFootball">
+        <v-card-title 
+        class="justify-center"
+        style="font-size: 16px; font-weight: bold">Football</v-card-title>
+      </v-card>
+      <v-card  class="mx-1 rounded-lg white--text deep-purple lighten-4"  width="30vw" @click="showBaseball">
+        <v-card-title class="justify-center" style="font-size: 16px; font-weight: bold">Baseball</v-card-title>
+      </v-card>
+      <v-card  class="mr-6 rounded-lg white--text deep-purple lighten-4" width="30vw" @click="showLOL">
+        <v-card-title class="justify-center" style="font-size: 15px; font-weight: bold">e-SPORTS</v-card-title>
+      </v-card>
     </v-row>
 
     <br>
@@ -12,7 +19,6 @@
       <football v-if="now === 'Football'"/>
       <baseball v-else-if="now === 'Baseball'"/>
       <lol v-else-if="now === 'LOL'"/>
-
     </div>
 
   </div>
--- Front/src/components/Sports/news/football.vue
+++ Front/src/components/Sports/news/football.vue
@@ -1,66 +1,21 @@
 <template>
   <div>
-    <v-sheet>
-      <v-carousel
-        cycle
-        height="300"
-        hide-delimiter-background
-        show-arrows-on-hover
-        light
-        hide-delimiters
-      >
-        <v-carousel-item
-          v-for="(post, i) in posts"
-          :key="i"
-        >
-          <v-sheet 
-            height="100%"
-          >
-          <a :href="post.link" target="_Blank">
-            <v-card
-              class="mx-auto"
-              max-width="300"
-            >
-                <v-img 
-                  :src="post.imgsrc"
-                  max-height="150"
-                ></v-img>
-
-              <v-card-title>
-                {{ post.title }}
-              </v-card-title>
-
-            </v-card>
-          </a>
-          </v-sheet>
-        </v-carousel-item>
-      </v-carousel>
-    </v-sheet>
     <div class="p-3">
       <v-row v-for="(post, idx) in posts" :key="idx">
         <v-col cols=12>
-          <a :href="post.link" target="_blank">
-            <v-card
-              color="blue lighten-5"
-            >
-              <div class="d-flex flex-no-wrap justify-space-between">
-                <div>
-                    <v-card-title class="headline">
-                      {{ post.title }}
-                    </v-card-title>
-                      <v-card-subtitle>{{post.content}}</v-card-subtitle>
-                </div>
-                  <v-avatar
-                      class="ma-3"
-                      size="125"
-                      tile
-                  >
-                    <v-img :src="post.imgsrc"></v-img>
-                  </v-avatar>
-
-              </div>
-            </v-card>
-          </a>
+          <a :href="post.link" target="_Blank">
+          <v-card
+            class="ml-3 rounded-xl"
+            max-width="335"
+          >
+              <v-img 
+                :src="post.imgsrc"
+              ></v-img>
+            <v-card-title>
+              {{ post.title }}
+            </v-card-title>
+          </v-card>
+        </a>
         </v-col>
       </v-row>
       <br>
