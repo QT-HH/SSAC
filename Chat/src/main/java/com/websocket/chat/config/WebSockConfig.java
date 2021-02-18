7a6eda91a4ff8d63f72dfd2e8b10ef1abfa31111
root <root@ip-172-26-3-9.ap-northeast-2.compute.internal>
2021-02-19 07:58:36 +0900

    0219 Chat update

--- Chat/src/main/java/com/websocket/chat/config/WebSockConfig.java
+++ Chat/src/main/java/com/websocket/chat/config/WebSockConfig.java
@@ -18,7 +18,7 @@ public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-stomp").setAllowedOrigins("http://i4d102.p.ssafy.io/")
+        registry.addEndpoint("/ws-stomp").setAllowedOrigins("*")
                 .withSockJS();
     }
 }
